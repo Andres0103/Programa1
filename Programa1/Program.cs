69c26d2b20c48dcd4fe56dcc61f39a9eeec67c6a
Usuario <Usuario@B13-211-5067>
2022-08-16 21:22:58 -0500

    Agregar archivos de proyecto.

---
+++ Programa1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //se llama la clase triangulo y se instancia el objeto
+            Triangulo t1 = new Triangulo();
+
+            Console.WriteLine("Digita el lado 1: ");
+            t1.l1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digita el lado 2: ");
+            t1.l2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digita base: ");
+            t1.b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digita altura: ");
+            t1.a = Convert.ToDouble(Console.ReadLine());
+
+            //Instanciar el metodo
+
+            t1.Area();
+            // imprimir el atributo
+            Console.WriteLine("El area es: " + t1.area.ToString());
+
+            //aqui se invoca el metodo, pero como el metodo retorna entonces trae el valor
+            Console.WriteLine("El perimetro es: " + t1.Perimetro().ToString());
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Programa1/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    internal class Triangulo
+    {
+        //declara variables con get y ser
+        public double l1 { get; set; }
+        public double l2 { get; set; }
+        public double b { get; set; }
+        public double a { get; set; }
+        public double area {get; set;}
+
+        //se declara el metodo constructor
+       public Triangulo()
+        {
+            l1 = 0;
+            l2 = 0;
+            b = 0;
+            a = 0;
+
+        }
+
+        // se crea el metodo Area, de tipo void porque no va a retornar.
+        public void Area()
+        {
+            area = b * a / 2;
+        }
+
+        // metodo para hayar perimetro
+        public double Perimetro()
+        {
+            return l1 + l2 + b;
+        }
+    }
+}
